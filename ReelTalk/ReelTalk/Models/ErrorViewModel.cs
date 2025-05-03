efa9abd92ff654dac8a5b9d91ccb337c4a28d6c0
Yanislav Baev <130275061+Yanislav07@users.noreply.github.com>
2025-05-03 11:29:52 +0300

    Delete ReelTalk directory

--- ReelTalk/ReelTalk/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReelTalk.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
